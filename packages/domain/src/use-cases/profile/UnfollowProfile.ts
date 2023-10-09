517b455a4af124188c103a26481e0ad0b596e3af
alexmance <alexmance@users.noreply.github.com>
2023-10-09 13:14:08 +0000

    Deploying to docs from @ alexmance/lens-sdk@d80f86a02487a97ea0968333f13536075c89ac7c 🚀

--- packages/domain/src/use-cases/profile/UnfollowProfile.ts
+++
@@ -1,9 +0,0 @@
-import { ProfileId, TransactionKind } from '../../entities';
-import { SubsidizeOnChain } from '../transactions/SubsidizeOnChain';
-
-export type UnfollowRequest = {
-  profileId: ProfileId;
-  kind: TransactionKind.UNFOLLOW_PROFILE;
-};
-
-export class UnfollowProfile extends SubsidizeOnChain<UnfollowRequest> {}
